de79d4f73d5f1966818de72a09cd8b9d2adea399
Kyle Hopfer <Kyle.Hopfer@gmail.com>
2017-10-28 20:57:31 -0500

    Added docker save to dockerize.sh

--- dockerize.sh
+++ dockerize.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 ng build --prod
 docker build -t gpls-ui:v0.1.0 .
+docker save -o /Users/kylehopfer/Desktop/gpls-ui.tar.gz gpls-ui
 
 # Local
 #docker run --restart=always --name gpls-ui -p 4200:80 -d gpls-ui:v0.1.0
