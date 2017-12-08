09999d1c054ae8c8a618d6c591c3151f4d617e21
Kyle Hopfer <Kyle.Hopfer@gmail.com>
2017-12-07 20:48:15 -0600

    Updated server address

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
 // Test URL
-export const GPLS_API_URL = "http://localhost:8080";
+// export const GPLS_API_URL = "http://localhost:8080";
 
 // Prod URL
-// export const GPLS_API_URL = "http://13.58.251.62:8080";
+export const GPLS_API_URL = "http://13.58.251.62:8080";
